566cd589f3ff63177daf0c68144a2624863b08c1
Graff Médéric <graffmederic@gmail.com>
2021-07-27 17:16:16 +0200

    Ajout du package Mongoose et connexion avec la base de données MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,19 @@
 // Importation du framework Node.JS Express
 const express = require('express');
 
+// Importation du package Mongoose pour faciliter les interactions avec notre base de données MongoDB
+const mongoose = require('mongoose');
+
 const bodyParser = require('body-parser');
 
 const app = express();
+
+// Connexion à la base de données MongoDB
+mongoose.connect('mongodb+srv://Admin_P6:OpenClassrooms_P6_AdminCode@clusterp6.3kyh0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 // Ajout des headers permettant le Cross Origin Resource Sharing (CORS)
 app.use((req, res, next) => {
@@ -11,8 +21,9 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); // Accès autorisé sous certains en-têtes
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // Accès autorisé sous certaines méthodes
     next();
-  });
+});
 
+// Traitement des données via bodyParser
 app.use(bodyParser.json());
 
 
