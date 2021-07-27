c464ee6ed8eda09d432077ddc116f5fda276841c
Graff Médéric <graffmederic@gmail.com>
2021-07-27 16:40:07 +0200

    Ajout du package body-parser

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,22 @@
+// Importation du framework Node.JS Express
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
+
+// Ajout des headers permettant le Cross Origin Resource Sharing (CORS)
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Accès autorisé pour tous
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); // Accès autorisé sous certains en-têtes
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // Accès autorisé sous certaines méthodes
+    next();
+  });
+
+app.use(bodyParser.json());
+
+
+
 
 app.use((req, res, next) => {
     console.log('Requête reçue !');
