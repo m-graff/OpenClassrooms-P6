832d049e5c5b0cfc7ba744342bc2de63048665ff
Graff Médéric <graffmederic@gmail.com>
2021-07-27 18:04:31 +0200

    Début de configuration du routage

---
+++ backend/routes/sauce.js
@@ -0,0 +1,13 @@
+// Création d'un routeur Express
+const express = require('express')
+const router = express.Router();
+
+// Différentes routes 
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.get('/', auth, sauceCtrl.getAllSauces);
+router.post('/:id/like', auth, sauceCtrl.likeSauce);
+
+module.exports = router;
