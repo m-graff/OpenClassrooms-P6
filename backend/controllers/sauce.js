798c81e30ffb1a708ed2cb0c9c6ebefd03d5f523
Graff Médéric <graffmederic@gmail.com>
2021-07-28 10:04:10 +0200

    Suite configuration contrôleurs: route récupération une / toutes les sauces

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -45,3 +45,17 @@ exports.deleteSauce = (req, res, next) => {
         })
         .catch(error => res.status(500).json({ error }));
 };
+
+// Récupération d'une sauce
+exports.getOneSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => res.status(200).json(sauce))
+        .catch(error => res.status(404).json({ error }));
+};
+
+// Récupération de toutes les sauces 
+exports.getAllSauces = (req, res, next) => {
+    Sauce.find()
+        .then(sauces => res.status(200).json(sauces))
+        .catch(error => res.status(400).json({ error }));
+};
